ad2d67a503dc397187719d727e12b8c30839f842
AlrikM <alrbri0804@skola.goteborg.se>
2024-11-28 18:07:12 +0100

    First commmit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,31 @@
+import se.yrgo.models.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	//G
+	/**
+	Check 2 Book objects if equal.
+	 */
+	@Test
+	public void test2EqualBooks() {
+		Book a = new Book(1,"a","","","",0);
+		Book b = new Book(1,"a","","","",0);
+		assertEquals(a,b);
+	}
+
+	//G
+	/**
+	Check 2 Book objects if NOT equal.
+	 */
+	@Test
+	public void test2NonEqualBooks() {
+		Book a = new Book(1,"a","","","",0);
+		Book b = new Book(2,"b","","","",0);
+		assertNotEquals(a,b);
+	}
+
+}
---
+++ src/test/java/LoanTest.java
@@ -0,0 +1,16 @@
+import se.yrgo.models.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class LoanTest {
+
+	//VG
+	@Test
+	public void testDueDate() {
+
+   }
+}
