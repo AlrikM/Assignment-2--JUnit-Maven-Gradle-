0c0de02fe531216255990627160784ea790ef7b1
AlrikM <alrbri0804@skola.goteborg.se>
2024-11-29 15:44:59 +0100

    Automatically runs tasks

---
+++ src/test/java/BookCatalogTest.java
@@ -0,0 +1,83 @@
+import se.yrgo.models.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Done
+ */
+public class BookCatalogTest {
+
+	private BookCatalog bc;
+	private Book book1;
+
+	/**
+	 * Constructor for test
+	 */
+	public BookCatalogTest() {
+		bc = new BookCatalog();
+		book1 = new Book(1,"Learning Java","","","",0);
+		bc.addBook(book1);
+	}
+
+	//G
+	/**
+	number of Books should be 1 after "addbook" function.
+	 */
+	@Test
+	public void testAddABook() {
+		assertEquals(1, bc.getNumberOfBooks());
+	}
+
+	//G
+	/**
+	Make sure equals Book object after "findbook" function.
+	 */
+	@Test
+	public void testFindBook() {
+        try {
+            assertEquals(bc.getBookArray()[0],bc.findBook("Learning Java"));
+        } catch (BookNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	//G
+	/**
+	-||- with insensitive case
+	 */
+	@Test
+	public void testFindBookIgnoringCase() {
+		try {
+			assertEquals(bc.getBookArray()[0],bc.findBook("learning java"));
+		} catch (BookNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	//G
+	/**
+	-||- with bonus spaces before and after specified "title" of Book obj
+	 */
+	@Test
+	public void testFindBookWithExtraSpaces() {
+		try {
+			assertEquals(bc.getBookArray()[0],bc.findBook("     learning java      "));
+		} catch (BookNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	//VG
+	// This test should throw BookNotFoundException in order to pass.
+	/**
+	 * @throws BookNotFoundException
+	 * Makes sure function will throw exception.
+	 */
+	@Test
+	public void testFindBookThatDoesntExist() throws BookNotFoundException {
+		assertThrows(BookNotFoundException.class, () -> bc.findBook("Learning  Java"));
+	}
+
+}
